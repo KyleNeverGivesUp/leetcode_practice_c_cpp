0711a90e61104979ca4e055294d08106e3b64bad
KyleAtASU <shunjieh@asu.edu>
2025-07-24 14:37:40 -0700

    update

--- singly_linkedlist_functions.c
+++ singly_linkedlist_functions.c
@@ -13,7 +13,10 @@ struct singly_linked_list {
 
 typedef struct singly_linked_list slist;
 
-void add_to_head(slist* slist, struct node* new_node) {
+void add_to_head(slist* slist, int data) {
+    struct node* new_node = malloc(sizeof(struct node));
+    new_node->data = data;
+    new_node->next = NULL;
     if (slist->head == NULL){
         slist->head = new_node;
         slist->tail = new_node;
@@ -36,16 +39,9 @@ int main(){
     slist* scores = malloc(sizeof(slist));  // 为 scores 分配内存
     scores->head = NULL;
     scores->tail = NULL;
-
-    struct node* new_node1 = malloc(sizeof(struct node));
-    struct node* new_node2 = malloc(sizeof(struct node));
-    struct node* new_node3 = malloc(sizeof(struct node));
-    new_node1->data = 1;
-    new_node2->data = 2;
-    new_node3->data = 3;
-    add_to_head(scores, new_node1);
-    add_to_head(scores, new_node2);
-    add_to_head(scores, new_node3);
+    add_to_head(scores, 1);
+    add_to_head(scores, 2);
+    add_to_head(scores, 3);
     print_list(scores);
     
     return 0;
