2ca5e36d3257a8dd4d442a51bd67dda13177038b
Hu <shh075@ieng6-202.ucsd.edu>
2025-11-08 01:58:48 -0800

    cse29

---
+++ CSE29/2.8_input_output.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    int x;
+    float pi;
+    char ch;
+//    scanf("%d%g",&x, &pi);
+//    printf("%d", x);
+    ch = getchar();
+    putchar(ch);
+}
+
