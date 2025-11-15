53e8d4d1cfa3af0dcfb1111270222cd56feb3f1f
KyleAtASU <shunjieh@asu.edu>
2025-11-15 04:29:01 -0800

    upload

--- 7.reverse-integer.cpp
+++ 7.reverse-integer.cpp
@@ -11,13 +11,17 @@ public:
         int pop = 0;
         int rev = 0;
         while (x !=0) {
-            pop = x % 10;
-            cout << pop << endl;
-            x = x / 10;
+
+            cout << "pop: "<< pop << endl;
+            cout << "x: " << x << endl;
             // if (x < INT_MIN || x > INT_MAX || pop >= 8 || pop<=-7) return 0;
-            if (x < INT_MIN || x > INT_MAX || rev * 10 < INT_MIN || rev * 10 > INT_MAX) return 0;
+            if (x < INT_MIN || x > INT_MAX || rev * 10 + pop < INT_MIN || rev * 10 + pop > INT_MAX) return 0;
+            pop = x % 10;
+            x = x / 10;
             rev = rev * 10 + pop;
+            cout << "rev: " << rev << endl;
         }
+        cout << "final rev: " << rev << endl;
         return rev;
     }
 };
