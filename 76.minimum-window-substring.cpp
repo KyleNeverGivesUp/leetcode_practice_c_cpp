4224ae5ebdd48440e4af23fd39b05e9e9aa50f49
KyleAtASU <shunjieh@asu.edu>
2025-11-16 01:16:01 -0800

    #76

--- 76.minimum-window-substring.cpp
+++ 76.minimum-window-substring.cpp
@@ -8,7 +8,30 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        s
+        int count[256];
+        int window[256];
+        int required = 1;
+        for ( char &cc : t){
+            count[cc]++;
+        }
+        int len = s.length();
+        for(int right=0; right< len; ++right){
+            char sc = s[right];
+            if (count[sc] !=0){
+                window[sc]++;
+            }
+            for ( char &cc : t){
+                if (window[cc] ==0){
+                    required = 0;
+                }
+            }
+        if (required == 1){
+            left++;
+        }
+
+
+        }
+        return ;
     }
 };
 // @lc code=end
