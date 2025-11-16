fae742ec26208de13b9a9578de3d8757ea1d29e3
KyleAtASU <shunjieh@asu.edu>
2025-11-15 16:08:03 -0800

    leetcode #20

--- 20.valid-parentheses.cpp
+++ 20.valid-parentheses.cpp
@@ -8,30 +8,55 @@
 class Solution {
 public:
     bool isValid(string s) {
-        std::stack<char> leftBrackets;
-        std::unordered_map<char, char> pairings = {
-            {')', '('},
-            {']', '['},
-            {'}', '{'}
-        };
+        // std::stack<char> leftBrackets;
+        // std::unordered_map<char, char> pairings = {
+        //     {')', '('},
+        //     {']', '['},
+        //     {'}', '{'}
+        // };
 
-        for(char c: s){
+        // for(char &c: s){
+        //     if (pairings.find(c) == pairings.end()){
+        //         leftBrackets.push(c);
+        //     }
+        //     else {
+        //         if (leftBrackets.empty()){
+        //             return false;
+        //         }
+
+        //         char topElement = leftBrackets.top();
+        //         leftBrackets.pop();
+
+        //         if (topElement != pairings.at(c)){
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return leftBrackets.empty();
+        std::stack<char> st;
+        std::unordered_map<char, char> pairings;
+        pairings.insert( { ')', '(' });
+        pairings.insert( { ']', '[' } );
+        pairings.insert( { '}', '}' } );
+
+        for(auto &c : s){
             if (pairings.find(c) == pairings.end()){
-                leftBrackets.push(c);
+                st.push(c);
             }
-            else {
-                if (leftBrackets.empty()){
+            else{
+                if (st.empty()){
                     return false;
                 }
-                char topElement = leftBrackets.top();
-                leftBrackets.pop();
-                
-                if (topElement != pairings.at(c)){
-                    return false;
+
+                char top_elements = st.top();
+                st.pop();
+                if (top_elements != pairings.at(c)){
+                        return false;
                 }
             }
         }
-        return leftBrackets.empty();
+        return st.empty();
+
     }
 };
 // @lc code=end
