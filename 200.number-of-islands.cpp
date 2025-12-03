b3cc8d959ffb9e4e6a6860f10e66fb80d56a8d96
KyleAtASU <shunjieh@asu.edu>
2025-12-02 18:46:18 -0800

    #200

--- 200.number-of-islands.cpp
+++ 200.number-of-islands.cpp
@@ -8,7 +8,27 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-         
+        int row = grid.size();
+        int col = grid[0].size();
+        int count = 0;
+        for (int i=0; i<row; ++i){
+            for (int j=0; j<col; ++j){
+                if (grid[i][j] == '1'){
+                    dfs(grid, i, j, row, col);
+                    count++;
+                }
+            }
+        }
+    return count;
+    }
+    void dfs(vector<vector<char>>& grid, int x, int y, int r, int c){
+        if(x < 0 || x>=r || y<0 || y>=c || grid[x][y]=='0') return;
+        grid[x][y] = '0';
+        dfs(grid, x+1, y, r, c);
+        dfs(grid, x-1, y, r, c);
+        dfs(grid, x, y+1, r, c);
+        dfs(grid, x, y-1, r, c);
+
     }
 };
 // @lc code=end
