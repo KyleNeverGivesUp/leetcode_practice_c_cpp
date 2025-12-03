2886822bcea4c43f42273e08046e4c763a6a006e
KyleAtASU <shunjieh@asu.edu>
2025-12-03 10:59:29 -0800

    153 done

--- 153.find-minimum-in-rotated-sorted-array.cpp
+++ 153.find-minimum-in-rotated-sorted-array.cpp
@@ -9,7 +9,7 @@ class Solution {
 public:
     int findMin(vector<int>& nums) {
         int left =0, right = nums.size() -1;
-        while (left < right){
+        while (left <= right){
             int mid = left + (right - left) /2;
             if (nums[mid] < nums[right]){
                 right = mid;
@@ -17,7 +17,7 @@ public:
             else
                 left = mid +1;
         }
-        return nums[left];
+        return nums[right];
     }
 };
 // @lc code=end
