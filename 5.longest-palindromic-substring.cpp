5975e014d82ed7e803e0ce9f0e3dc12ac7ae156f
KyleAtASU <shunjieh@asu.edu>
2025-12-07 11:11:44 -0800

    5 done

---
+++ 5.longest-palindromic-substring.cpp
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=5 lang=cpp
+ *
+ * [5] Longest Palindromic Substring
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int max_len = 1;
+        int start =0;
+        int n = s.size();
+        for(int i=0; i<n; i++){
+            int odd_len = search(s, i ,i);
+            int even_len = search(s, i, i+1);
+            int curr_len = max(odd_len, even_len);
+            if (curr_len > max_len ){
+                max_len = curr_len;
+                start = i - (curr_len -1)/2;
+            }
+        
+        }
+        return s.substr(start, max_len);
+    }
+
+    int search(string s, int left, int right){
+        while(left >= 0 && right < s.size() && s[left] == s[right]){
+            left--;
+            right++;
+        }
+        return right - left -1;
+    }
+};
+// @lc code=end
+
