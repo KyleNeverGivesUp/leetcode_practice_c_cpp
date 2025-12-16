4363cd5ff72de191a0aa6fe0bb7b08e9e20459d5
KyleAtASU <shunjieh@asu.edu>
2025-12-16 11:22:49 -0800

    #26 done

--- 26.remove-duplicates-from-sorted-array.cpp
+++ 26.remove-duplicates-from-sorted-array.cpp
@@ -8,7 +8,14 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        
+        int insertIndex = 1;
+        for (int i=1; i<nums.size(); ++i){
+            if (nums[i] != nums[i-1]){
+                nums[insertIndex] = nums[i];
+                insertIndex++;
+            }
+        }
+        return insertIndex;
     }
 };
 // @lc code=end
