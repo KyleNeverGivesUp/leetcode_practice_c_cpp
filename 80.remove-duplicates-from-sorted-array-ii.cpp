a100e377efb01ef7da2e12d9a7df74d299ce4981
KyleAtASU <shunjieh@asu.edu>
2025-12-16 16:08:23 -0800

    #80 done

--- 80.remove-duplicates-from-sorted-array-ii.cpp
+++ 80.remove-duplicates-from-sorted-array-ii.cpp
@@ -8,7 +8,16 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        
+        int insertIndex = 1;
+        int count = 1;
+        for (int i=1; i<nums.size(); i++){
+            if (nums[i] == nums[i-1]) count++;
+            else count = 1;
+            if (count <= 2){
+                nums[insertIndex++] = nums[i];
+            }
+        }
+        return insertIndex;
     }
 };
 // @lc code=end
