f06256a228f0b1e08493c322674ad42cc901dcb8
KyleAtASU <shunjieh@asu.edu>
2025-12-19 12:16:17 -0800

    allocator

--- myAllocator_Kyle.c
+++ myAllocator_Kyle.c
@@ -6,20 +6,20 @@
 #define HEAP_SIZE 400
 #define VAL_SIZE 8
 
-uint64_t* HEAP_START = NULL;
+uint64_t* HEAP_START;
 
 void init_heap(){
-    uint64_t* heap = mmap(NULL, HEAP_SIZE, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+    uint64_t* heap = mmap(NULL, HEAP_SIZE, PROT_WRITE | PROT_READ, MAP_SHARED | MAP_ANON, -1, 0);
     HEAP_START = heap;
     *HEAP_START = HEAP_SIZE - VAL_SIZE;
 }
 
-void* malloc(size_t size){
+void* my_malloc(size_t size){
     uint64_t* current = HEAP_START;
-    while (current < (HEAP_START + (HEAP_SIZE / VAL_SIZE))){
+    while(current < HEAP_START + (HEAP_SIZE / VAL_SIZE)) {
         uint64_t cur_header = *current;
-        uint64_t cur_size = (cur_header / 2) * 2;
-        if ((cur_header % 2 == 0) && (size <= cur_size)){
+        uint64_t cur_size = (cur_header /2) * 2;
+        if ((cur_header % 2 ==0) && (size <= cur_size)){
             size_t rounded = ((size + 7) / 8) * 8;
             *current = rounded + 1;
 
@@ -28,7 +28,7 @@ void* malloc(size_t size){
             *remaining_ptr = remaining;
             return current + 1;
         }
-        else {
+        else{
             uint64_t* next = current + (cur_size / VAL_SIZE) + 1;
             current = next;
         }
@@ -38,7 +38,7 @@ void* malloc(size_t size){
 
 int main(){
     init_heap();
-    int* a = malloc(40);
+    int* a = my_malloc(40);
     printf("%llu %llu\n", HEAP_START[0], HEAP_START[1]);
     return 0;
 }
