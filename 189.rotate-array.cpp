ae88e59fa8b829d37e5861a86db7ad0d10285753
KyleAtASU <shunjieh@asu.edu>
2025-12-23 03:15:16 -0800

    #189 done

--- 189.rotate-array.cpp
+++ 189.rotate-array.cpp
@@ -35,19 +35,33 @@ public:
         // }
 
         // solution Cyclic Replacements
-        int temp=0, prev=0, current=0, count=0, start = 0;
+        // int temp=0, prev=0, current=0, count=0, start = 0;
+        // while(count < size){
+        //     current = start; prev = nums[start];
+        //     do{
+        //         int next = (current + k) % size;
+        //         temp = nums[next];
+        //         nums[next] = prev;
+        //         prev = temp;
+        //         current = next;
+        //         count++;
+        //     }while( current != start);
+        //     start++;
+        // }
+        int current = 0, start = 0, count = 0, temp, prev = 0;
         while(count < size){
             current = start; prev = nums[start];
             do{
-                int next = (current + k) % size;
-                temp = nums[next];
-                nums[next] = prev;
-                prev = temp;
-                current = next;
-                count++;
-            }while( current != start);
+                    int next = (current + k) % size;
+                    temp = nums[next];
+                    nums[next] = prev;
+                    prev = temp;
+                    current = next;
+                    count++;
+            }while(current != start);
             start++;
         }
+
     }
 };
 // @lc code=end
