e1f9a2fecbe6c0f291fae13871a094eb88af429c
KyleAtASU <shunjieh@asu.edu>
2026-01-01 08:19:56 -0800

    #122 approach 2

--- 122.best-time-to-buy-and-sell-stock-ii.cpp
+++ 122.best-time-to-buy-and-sell-stock-ii.cpp
@@ -8,19 +8,29 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        // Approach 1
+        // int total = 0;
+        // int valley = INT_MAX;
+        // int peak = INT_MAX;
+        // for(int i=0;i<prices.size();i++){
+        //     if (prices[i] < peak){
+        //         total += peak - valley;
+        //         valley = prices[i];
+        //         peak = valley;
+        //     }else{
+        //         peak = prices[i];
+        //     }
+        // }
+        // return total += peak - valley;
+        // Approach 2
         int total = 0;
-        int valley = INT_MAX;
-        int peak = INT_MAX;
-        for(int i=0;i<prices.size();i++){
-            if (prices[i] < peak){
-                total += peak - valley;
-                valley = prices[i];
-                peak = valley;
-            }else{
-                peak = prices[i];
+        for (int i=1; i<prices.size(); i++){
+            if (prices[i] > prices[i-1]){
+                total += prices[i] - prices[i-1];
             }
         }
-        return total += peak - valley;
+        return total;
+
     }
 };
 // @lc code=end
