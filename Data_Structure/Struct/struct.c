83835823bad5897e46b5da5b4b26083d8fd4749f
KyleAtASU <shunjieh@asu.edu>
2026-02-15 16:15:17 -0800

    practice

---
+++ Data_Structure/Struct/struct.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+// struct Student{
+//     char name[50];
+//     int id;
+//     float gpa;
+// };
+
+typedef struct{
+    char name[50];
+    int id;
+    float gpa;
+} Student;
+
+int main(){
+    Student s1;
+    s1.id = 101;
+    s1.gpa = 3.8;
+    strcpy(s1.name, "Kyle");
+    printf("ID is %d\n", s1.id);
+
+    Student s2 = {"Xinyi", 102, 3.9};
+    printf("ID is %s\n", s2.name);
+    
+    Student *p = &s1;
+    printf("ID is %d\n", p->id);
+
+    return 0; 
+}
