b1b51c56d04d9feb0a34c2f8ccce3c86c10262c9
KyleAtASU <shunjieh@asu.edu>
2026-02-15 20:06:43 -0800

    linkedlist

--- Data_Structure/Struct/node-struct.c
+++ Data_Structure/Struct/node-struct.c
@@ -1,4 +1,74 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> // 为了用 malloc
 
-typedef struct N
+typedef struct Node{
+    int data;
+    struct Node* next;
+} Node;
+
+typedef struct linkedList{
+    Node* head;
+    Node* tail;
+    int size;
+}ll;
+
+Node* createNode(int value){
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void initialll(ll* list){
+    list->head = NULL;
+    list->tail = NULL;
+    list->size = 0;
+}
+
+void append(ll* list, int value){
+    Node* n = malloc(sizeof(Node));
+    n->data = value;
+    n->next = NULL;
+    
+    if (list->head == NULL){
+        list->head = n;
+        list->tail = n;
+    }else{
+        list->tail->next = n;
+        list->tail = n;
+    }
+    list->size++;
+}
+
+void freeList(ll* list){
+    Node* curr = list->head;
+    while (curr != NULL){
+        Node* next = curr->next;
+        free(curr);
+        curr = next;
+    }
+
+    list->head = NULL;
+    list->tail = NULL;
+    list->size = 0;
+}
+
+
+int main(){
+    Node* myNode = createNode(42);
+    printf("Node data: %d\n", myNode->data);
+    ll l;
+    initialll(&l);
+    append(&l, 3);
+    append(&l, 5);
+
+    Node* curr = l.head;
+    while(curr != NULL){
+        printf("%d ", curr->data);
+        curr = curr->next;
+    }
+    printf("total size is: %d\n", l.size);
+    freeList(&l);
+    printf("total size is: %d\n", l.size);
+    return 0;
+}
--- Data_Structure/Struct/struct.c
+++ Data_Structure/Struct/struct.c
@@ -13,6 +13,12 @@ typedef struct{
     float gpa;
 } Student;
 
+typedef struct {
+    char name[50];
+    int id;
+    float gpa;
+} Teacher ;
+
 int main(){
     Student s1;
     s1.id = 101;
@@ -20,8 +26,8 @@ int main(){
     strcpy(s1.name, "Kyle");
     printf("ID is %d\n", s1.id);
 
-    Student s2 = {"Xinyi", 102, 3.9};
-    printf("ID is %s\n", s2.name);
+    // Teacher t1 = {"Xinyi", 102, 3.9};
+    // printf("Teacher ID is %s\n", t1.name);
     
     Student *p = &s1;
     printf("ID is %d\n", p->id);
