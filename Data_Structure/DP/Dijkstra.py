f876ce0f0aed86655556a8b9d0ec21d00b0c1d16
KyleAtASU <shunjieh@asu.edu>
2026-03-25 03:51:13 -0700

    dijkstra implemented

--- Data_Structure/DP/Dijkstra.py
+++ Data_Structure/DP/Dijkstra.py
@@ -1,27 +1,49 @@
-from collections import deque
-def dijkstra(graph, s, t):
-    if s not in visited:
-        queue = deque([s])
-        visited.append(s)
-        
+import heapq
 
+def dijkstra(graph, start: int):
+    
+    # dist = {node: float('inf') for node in graph}
+    # dist[start] = 0
+    # heap = [(0, start)]
+
+    # while heap:
+    #     d, u = heapq.heappop(heap)
+    #     if d > dist[u]:
+    #         continue
+
+    #     for v, weight in graph[u]:
+    #         if dist[u] + weight < dist[v]:
+    #             dist[v] = dist[u] + weight
+    #             heapq.heappush(heap, (dist[v], v))
+    
+    # return dist
+    dist = {node : float('inf') for node in graph}
+    dist[start] = 0
+    heap = [(0, start)]
+    while heap:
+        d, u = heapq.heappop(heap)
+        if d > dist[u]:
+            continue
+
+        for v, weight in graph[u]:
+            if dist[v] > weight + dist[u]:
+                dist[v] = weight + dist[u]
+                heapq.heappush(heap, (dist[v], v))
+    return dist
 
 
 if __name__ == "__main__":
     # n = 节点数, edges = [[u, v, weight], ...]
-    n = 5
-    edges = [[0,1,2],[0,2,4],[1,2,1],[1,3,7],[2,4,3],[3,4,1]]
+    graph = {
+    'A': [('B', 1), ('C', 4)],
+    'B': [('C', 2), ('D', 5)],
+    'C': [('D', 1)],
+    'D': []
+}
 
-    # graph = {i:[] for i in range(n)}
-    # for u, v, w in edges:
-    #     graph[u].append((v, w))
-    # print(graph)
-
-    visited = []
-
-    graph = {i : [] for i in range(n)}
-    for u, v, w in edges:
-        graph[u].append((v, w))
-    print(graph)
+    # heap = [(1, 'A')]
+    # heapq.heappush(heap, (1, 'A'))
+    print(dijkstra(graph, 'A'))
+    
 
     
