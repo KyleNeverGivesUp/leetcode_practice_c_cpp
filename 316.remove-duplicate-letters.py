28bc7f25346d6c81433d299269d2eadf03081d13
KyleAtASU <shunjieh@asu.edu>
2026-04-05 16:08:01 -0700

    Fix return value in removeDuplicateLetters function to return a string instead of a list

--- 316.remove-duplicate-letters.py
+++ 316.remove-duplicate-letters.py
@@ -11,6 +11,7 @@ class Solution:
         stack = []
         visited = set()
         for i in s:
+            remain_count[i] -= 1
             if i in visited:
                 continue
             while stack and stack[-1] > i and remain_count[stack[-1]] > 0:
@@ -18,7 +19,7 @@ class Solution:
                 visited.remove(removed_char)
             stack.append(i)
             visited.add(i)
-        return stack
+        return "".join(stack)
         
 # @lc code=end
 
